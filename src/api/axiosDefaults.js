503c88a5a2232ce7a72b8332e54bd97fd8d15573
Seán O'Brien <sean95ob@gmail.com>
2024-04-11 16:38:34 +0000

    Add Axios interceptors to keep user logged in for longer periods

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -3,3 +3,6 @@ import axios from "axios";
 axios.defaults.baseURL = "https://travelers-tails-api-d0dd6ea40a9c.herokuapp.com";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
