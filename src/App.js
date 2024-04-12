16d6b118d1b271f5950d7ade0fc658e178a562c2
Seán O'Brien <sean95ob@gmail.com>
2024-04-12 15:03:24 +0000

    Add post feed and created liked posts section

--- src/App.js
+++ src/App.js
@@ -9,9 +9,13 @@ import signUpImage from "./assets/signupimage.jpg";
 import SignInForm from "./pages/auth/SignInForm";
 import PostCreateForm from "./pages/posts/PostCreateForm";
 import PostPage from "./pages/posts/PostPage";
+import PostsPage from "./pages/posts/PostsPage";
+import { useCurrentUser } from "./contexts/CurrentUserContext";
 
 
 function App() {
+  const currentUser = useCurrentUser();
+  const profile_id = currentUser?.profile_id || "";
   const [showSignUpImage, setShowSignUpImage] = useState('none');
 
   return (
@@ -21,9 +25,35 @@ function App() {
             style={{ display: showSignUpImage, position: 'absolute', width: '100vw', objectFit: 'cover', minHeight: 'calc(100vh + 105px)' }} alt="background"
           />
           <NavBar />
-          <Container className={styles.Main}>
-            <Switch>
-              <Route exact path="/" render={() => <h1>Home page</h1>} />
+            <Container className={styles.Main}>
+              <Switch>
+                <Route
+                exact
+                path="/"
+                render={() => (
+                  <PostsPage message="No results found. Adjust the search keyword." />
+                )}
+              />
+              <Route
+                exact
+                path="/feed"
+                render={() => (
+                  <PostsPage
+                    message="No results found. Adjust the search keyword or follow a user."
+                    filter={`owner__followed__owner__profile=${profile_id}&`}
+                  />
+                )}
+              />
+              <Route
+                exact
+                path="/liked"
+                render={() => (
+                  <PostsPage
+                    message="No results found. Adjust the search keyword or like a post."
+                    filter={`likes__owner__profile=${profile_id}&ordering=-likes__created_at&`}
+                  />
+                )}
+              />
               <Route exact path="/signin" render={() => <SignInForm setShowSignUpImage={setShowSignUpImage} />} />
               <Route exact path="/signup" render={() => <SignUpForm setShowSignUpImage={setShowSignUpImage} />} />
               <Route exact path="/posts/create" render={() => <PostCreateForm />} />
