ec9d9d6939cc6ac21b20f19bd5355ae74b1d1167
Seán O'Brien <sean95ob@gmail.com>
2024-04-12 12:04:31 +0000

    Setup basic structure of post create form and added some basic styling

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 Photo credit
 Purple Plane for logo and favicon taken from FedEx: https://www.fedex.com/en-us/holiday/last-days-to-ship.html 
 Purple sky image on signup tajen from https://www.pixel4k.com/purple-palm-tree-4k-55080.html 
+Upload image taken from Vecteezy: https://www.vecteezy.com/vector-art/424804-upload-icon-vector-illustration 
--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import "./api/axiosDefaults";
 import SignUpForm from "./pages/auth/SignUpForm";
 import signUpImage from "./assets/signupimage.jpg";
 import SignInForm from "./pages/auth/SignInForm";
+import PostCreateForm from "./pages/posts/PostCreateForm";
 
 
 function App() {
@@ -24,6 +25,7 @@ function App() {
               <Route exact path="/" render={() => <h1>Home page</h1>} />
               <Route exact path="/signin" render={() => <SignInForm setShowSignUpImage={setShowSignUpImage} />} />
               <Route exact path="/signup" render={() => <SignUpForm setShowSignUpImage={setShowSignUpImage} />} />
+              <Route exact path="/posts/create" render={() => <PostCreateForm />} />
               <Route render={() => <p>Page not found!</p>} />
             </Switch>
           </Container>
